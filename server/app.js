ca1b14fb231e73d7158a8a08837bf06ea6b1bce5
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-05-14 23:53:56 +0700

    made the structure of quiz page

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded( { extended: true }));
 
